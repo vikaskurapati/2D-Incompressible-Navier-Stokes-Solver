b35d97c456615507c2726dcfd7842cb221550afa
Kartik Bali <kartik.bali@tum.de>
2022-05-05 15:28:02 +0200

    Applied Boundary Conditions using skeleton provided

--- src/Boundary.cpp
+++ src/Boundary.cpp
@@ -1,36 +1,62 @@
 #include "Boundary.hpp"
 #include <cmath>
 #include <iostream>
-
+#include "Grid.hpp"
 FixedWallBoundary::FixedWallBoundary(std::vector<Cell *> cells) : _cells(cells) {}
 
 FixedWallBoundary::FixedWallBoundary(std::vector<Cell *> cells, std::map<int, double> wall_temperature)
     : _cells(cells), _wall_temperature(wall_temperature) {}
 
 void FixedWallBoundary::apply(Fields &field) {
-    int imax = field.p_matrix().imax();
-    int jmax = field.p_matrix().jmax();
 
-    for (int j = 0; j < jmax; j++)
-    {
-        field.u(0,j) = 0.0;
-        field.u(imax-1,j) = 0.0;
-        field.u(imax-2,j) = 0.0;
-        field.v(0,j) = -field.v(1,j);
-        field.v(imax-1, j) = -field.v(imax-2,j);
-        field.p(0, j) = field.p(1, j);
-        field.p(imax-1, j) = field.p(imax-2, j);
-        field.f(0,j) = 0.0;
-        field.f(imax-1, j) = field.u(imax-1, j);
-    }
+    unsigned int i,j;
 
-    for (int i = 0; i < imax; i++)
-    {
-        field.v(i,0) = 0.0;
-        field.u(i, 0) = -field.u(i,1);
-        field.p(i,0) = field.p(i,1);
-        field.g(i, 0) = field.v(i, 0);
+    for(auto& cell : _cells){
+        i = cell->i();
+        j = cell->j();
+
+        if(cell->is_border(border_position::RIGHT)){
+            field.u(i,j) = 0.0;
+            field.v(i,j) = - field.v(i+1,j);
+            field.p(i,j) = field.p(i+1,j);
+            field.f(i,j) = field.u(i,j);
+        }
+        if(cell->is_border(border_position::LEFT)){
+            field.u(i-1,j) = 0.0;
+            field.v(i,j) = - field.v(i-1,j);
+            field.p(i,j) = field.p(i-1,j);
+            field.f(i-1,j) = 0.0;
+        }
+        if(cell->is_border(border_position::TOP)){
+            field.u(i,j) = -field.u(i,j+1);
+            field.v(i,j) = 0.0;
+            field.p(i,j) = field.p(i,j+1);
+            field.g(i,j) = field.v(i,j);
+        }
+
+        //std:: cout << cell->is_border(border_position::BOTTOM) << " ";
     }
+    // for (int j = 0; j < jmax; j++)
+    // {
+        
+    //     field.u(0,j) = 0.0;
+    //     field.u(imax-1,j) = 0.0;
+    //     field.u(imax-2,j) = 0.0;
+    //     field.v(0,j) = -field.v(1,j);
+    //     field.v(imax-1, j) = -field.v(imax-2,j);
+    //     field.p(0, j) = field.p(1, j);
+    //     field.p(imax-1, j) = field.p(imax-2, j);
+    //     field.f(0,j) = 0.0;
+    //     field.f(imax-1, j) = field.u(imax-1, j);
+    // }
+
+    // for (int i = 0; i < imax; i++)
+    // {
+    //     field.v(i,0) = 0.0;
+    //     field.u(i, 0) = -field.u(i,1);
+    //     field.p(i,0) = field.p(i,1);
+    //     field.g(i, 0) = field.v(i, 0);
+    // }
 }
 
 
@@ -43,14 +69,19 @@ MovingWallBoundary::MovingWallBoundary(std::vector<Cell *> cells, std::map<int, 
     : _cells(cells), _wall_velocity(wall_velocity), _wall_temperature(wall_temperature) {}
 
 void MovingWallBoundary::apply(Fields &field) {
-    int imax = field.p_matrix().imax();
-    int jmax = field.p_matrix().jmax();
-    for (int i = 0; i < imax; i++)
-    {
-        field.u(i, jmax-1) = 2.0*_wall_velocity[LidDrivenCavity::moving_wall_id] -field.u(i, jmax-2);
-        field.v(i, jmax-1) = 0.0;
-        field.v(i, jmax-2) = 0.0;
-        field.p(i, jmax-1) = field.p(i, jmax-2);
-        field.g(i, jmax-1) = field.v(i, jmax-1);
+    unsigned int i,j;
+    //int jmax = field.p_matrix().jmax();
+    for(auto& cell : _cells){
+        
+        if(cell->is_border(border_position::BOTTOM)){
+            i = cell->i();
+            j = cell->j();
+            field.u(i,j) = 2.0 - field.u(i,j-1);
+            field.v(i,j) = 0.0;
+            field.p(i,j) = field.p(i,j-1);
+            field.g(i,j) = field.v(i,j);
+        }
+            
     }
+    //std:: cout << std::endl;
 }
