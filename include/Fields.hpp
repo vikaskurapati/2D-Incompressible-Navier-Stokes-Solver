bf5ddf150f4c4607e70432cdc0e57dbc6ed3126a
Surya Vamshi Penumarthi <p.suryavamshi5@gmail.com>
2022-05-06 14:04:35 +0200

    Added Documentation for calculate_fluxes function

--- include/Fields.hpp
+++ include/Fields.hpp
@@ -32,6 +32,23 @@ class Fields {
      * direction based on explicit discretization of the momentum equations
      *
      * @param[in] grid in which the fluxes are calculated
+     * 
+     * $F(i, j) = U(i,j) + dt*(nu*(laplacian(U,i,j)) - convection_u(U,V,i,j) + gx)$
+     * $G(i, j) = V(i,j) + dt*(nu*(laplacian(V,i,j)) - convection_v(U,V,i,j) + gy)$
+     * 
+     * Where convection_u and convection_v are defined in Discretization
+     * 
+     * $convection_u = du2/dx + duv/dy$
+     * $convection_v = duv/dx + dv2/dy$
+     * 
+     * Also, boundary conditions for fluxes were also updated
+     * 
+     * $F(0,j) = U(0,j)$
+     * $F(imax, j) = U(imax, j)$
+     * $G(i,0) = V(i,0)$
+     * $G(i, jmax) = V(i, jmax)$
+     * 
+     * Refer Equations 9, 10 and 17 in Worksheet
      *
      */
     void calculate_fluxes(Grid &grid);
@@ -41,6 +58,8 @@ class Fields {
      * Poisson equation
      *
      * @param[in] grid in which the calculations are done
+     * 
+     * $_RS(i,j) = (((_F(i,j)-_F(i-1,j))/dx)+((_G(i,j)-_G(i,j-1))/dy))/_dt$
      *
      */
     void calculate_rs(Grid &grid);
