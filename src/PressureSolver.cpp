0aba83cde6ff84af8f92916c65a11e838f0c33b6
Vikas Kurapati <vikas.kurapati@tum.de>
2022-06-30 10:59:19 +0200

    Modified Conjugate Gradient to factor in obstacles

--- src/PressureSolver.cpp
+++ src/PressureSolver.cpp
@@ -195,10 +195,12 @@ double ConjugateGradient::solve(Fields &field, Grid &grid, const std::vector<std
     imax = field.p_matrix().imax();
     jmax = field.p_matrix().jmax();
 
-    for (int i = 1; i < imax - 1; ++i) {
-        for (int j = 1; j < jmax - 1; ++j) {
+    for (auto currentCell : grid.fluid_cells()) {
+        int i = currentCell->i();
+        int j = currentCell->j();
+        if (i != 0 && j != 0 && i != grid.domain().size_x + 1 && j != grid.domain().size_y + 1) {
             double delta = Discretization::laplacian(pressure, i, j);
-            residual(i, j) = rhs(i, j) - delta;
+            residual(i, j) - rhs(i, j) - delta;
         }
     }
 
@@ -208,8 +210,10 @@ double ConjugateGradient::solve(Fields &field, Grid &grid, const std::vector<std
 
     Matrix<double> q(imax, jmax, 0.0);
 
-    for (int i = 1; i < imax - 1; ++i) {
-        for (int j = 1; j < jmax - 1; ++j) {
+    for (auto currentCell : grid.fluid_cells()) {
+        int i = currentCell->i();
+        int j = currentCell->j();
+        if (i != 0 && j != 0 && i != grid.domain().size_x + 1 && j != grid.domain().size_y + 1) {
             q(i, j) =
                 Discretization::laplacian(d, i, j); // the q = Ad for this system would be laplacian of the d vector
         }
@@ -219,8 +223,10 @@ double ConjugateGradient::solve(Fields &field, Grid &grid, const std::vector<std
     double alpha_den = 0;
     double alpha = 0;
 
-    for (int i = 0; i < imax; ++i) {
-        for (int j = 0; i < jmax; ++j) {
+    for (auto currentCell : grid.fluid_cells()) {
+        int i = currentCell->i();
+        int j = currentCell->j();
+        if (i != 0 && j != 0 && i != grid.domain().size_x + 1 && j != grid.domain().size_y + 1) {
             alpha_num += residual(i, j) * residual(i, j); // num of alpha is delta_new = r^T * r
             alpha_den += d(i, j) * q(i, j);               // den of alpha is d^T * q
         }
@@ -232,8 +238,11 @@ double ConjugateGradient::solve(Fields &field, Grid &grid, const std::vector<std
     double beta_num = 0;
     double beta = 0;
 
-    for (int i = 0; i < imax; i++) {
-        for (int j = 0; j < jmax; j++) {
+    for (auto currentCell : grid.fluid_cells()) {
+        int i = currentCell->i();
+        int j = currentCell->j();
+        if (i != 0 && j != 0 && i != grid.domain().size_x + 1 && j != grid.domain().size_y + 1) {
+
             field.p(i, j) = pressure(i, j) + alpha * d(i, j);
             beta_den += residual(i, j) * residual(i, j);
             residual(i, j) -= alpha * q(i, j);
@@ -243,8 +252,10 @@ double ConjugateGradient::solve(Fields &field, Grid &grid, const std::vector<std
 
     beta = beta_num / beta_den;
 
-    for (int i = 0; i < imax; i++) {
-        for (int j = 0; j < jmax; j++) {
+    for (auto currentCell : grid.fluid_cells()) {
+        int i = currentCell->i();
+        int j = currentCell->j();
+        if (i != 0 && j != 0 && i != grid.domain().size_x + 1 && j != grid.domain().size_y + 1) {
             d(i, j) = residual(i, j) + beta * d(i, j);
         }
     }
